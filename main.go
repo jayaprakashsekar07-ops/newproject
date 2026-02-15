0781fc53ee6032eac851c69cf40ca09f85bc5e69
jayaprakashsekar07-ops <jayaprakashsekar07@gmail.com>
2026-02-15 15:38:57 +0530

    new_update

--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	ch := make(chan int)
-	ch1 := make(chan string)
-	go sayHello(ch)
-	go Hello(ch1)
-
-	res := <-ch
-	fmt.Println(res)
-	res1 := <-ch1
-	fmt.Println(res1)
-
-}
-
-func sayHello(ch chan int) {
-	ch <- 10
-
-}
-
-func Hello(ch chan string) {
-
-	ch <- "Hii From Hello Goroutine"
 
 }
