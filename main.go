875c03130b36231c5d07fb7e778faca16267c689
jayaprakashsekar07-ops <jayaprakashsekar07@gmail.com>
2026-02-18 07:44:33 +0530

    update on 18.02.2026

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	// Basic types
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	// Pointer
+	var p *int
+
+	// Slice
+	var s []int
+
+	// Map
+	var m map[string]int
+
+	m["a"] = 10
+
+	fmt.Println("int:", a)
+	fmt.Println("float:", b)
+	fmt.Println("string:", c)
+	fmt.Println("bool:", d)
+	fmt.Println("pointer:", p)
+	fmt.Println("slice:", s)
+	fmt.Println("map:", m)
 }
